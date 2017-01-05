6af3b7bb8ee370ee9a5d9e3492f03a3591c7f90e
KenLomax <ken.lomax@sap.com>
2017-01-05 13:50:28 +0100

    TddTrail updtaes

--- scriptForGoCD/scriptForGoCD.sh
+++ scriptForGoCD/scriptForGoCD.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rm ../../hybris-commerce-suite-6.2.0.1GO
+rm ../../hybris-commerce-suite-6.2.0.1
 unzip ../../hybris-commerce-suite-6.2.0.1.zip -d ../../hybris-commerce-suite-6.2.0.1
 #cp -r ../../hybris-commerce-suite-6.2.0.1ORIG ../../hybris-commerce-suite-6.2.0.1
-cd ../../hybris-commerce-suite-6.2.0.1/Installer; ./install.sh -r b2b_acc
+cd ../../hybris-commerce-suite-6.2.0.1/Installer; ./install.sh -r b2b_acc setup
+./install.sh -r b2b_acc initialize; ./install.sh -r b2b_acc start
 cd ../hybris/bin/platform;  . ./setantenv.sh; ant extgen -Dinput.template="yempty" -Dinput.name="tddtrail" -Dinput.package="com.hybris.tddtrail"
 sed -i '' 's/<\/extensions>/<extension dir="..\/custom\/tddtrail"\/><\/extensions>/' ../../config/localextensions.xml
 ant clean all
