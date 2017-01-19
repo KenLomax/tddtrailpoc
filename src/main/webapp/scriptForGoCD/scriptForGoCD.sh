87b009fab5674076b1b9b0512dac4bc85a7e9b7b
KenLomax <ken.lomax@sap.com>
2017-01-19 16:37:43 +0100

    refactoring

--- src/main/webapp/scriptForGoCD/scriptForGoCD.sh
+++
@@ -1,4 +0,0 @@
-cd /Users/d061192/trail
-unzip hybris-commerce-suite-6.2.0.1.zip -d hybris-commerce-suite-6.2.0.1
-cd /Users/d061192/trail/TddTrail
-mvn -Dtest=TrailSetupTest#testTddTrailPrerequisites test|| { echo "Test testTddTrailPrerequisites Unsuccessful" }
